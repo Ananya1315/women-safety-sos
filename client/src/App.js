97990f2b628cbdb89571e501bbf774fcab2c1620
neeharika-kuralla <mahalakshmineeharika@gmail.com>
2026-02-09 11:40:31 +0530

    added trusted circles page with database

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,16 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import UserSOSPage from "./pages/UserSOS";
+import TrustedCircle from "./pages/TrustedCircle";
 
 function App() {
-  const [status, setStatus] = useState("");
-
-  const handleSOS = () => {
-    if (!navigator.geolocation) {
-      setStatus("Geolocation not supported");
-      return;
-    }
-
-    setStatus("Sending SOS...");
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        fetch("http://localhost:5000/api/sos", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-            triggertype:"normal",
-            method:"button"
-          }),
-        })
-          .then((res)=>res.json())
-          .then((data) => setStatus("🚨 SOS Sent Successfully"))
-          .catch(() => setStatus("Failed to send SOS"));
-      },
-      () => setStatus("Location permission denied")
-    );
-  };
-
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Women Safety Platform</h1>
-
-      <button
-        onClick={handleSOS}
-        style={{
-          padding: "30px",
-          fontSize: "24px",
-          backgroundColor: "red",
-          color: "white",
-          borderRadius: "50%",
-          border: "none",
-          cursor: "pointer",
-        }}
-      >
-        SOS
-      </button>
-
-      <p>{status}</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserSOSPage />} />
+        <Route path="/trusted" element={<TrustedCircle />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
